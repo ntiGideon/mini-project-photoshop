263df4b37f0c7b538aa287530e33a28009090b08
ntiGideon <gideon.boateng@amalitech.com>
2025-06-23 12:07:51 +0000

    first commit

---
+++ src/test/java/com/photoshop/fotoshop/FotoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photoshop.fotoshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FotoShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
